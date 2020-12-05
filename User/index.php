3e9e5054f0b28482dc0ba2f9b34fb761de839c4f
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2020-12-05 17:50:23 +0700

    User - Index.php

--- User/index.php
+++ User/index.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['user_login'])) {
     <script src="https://kit.fontawesome.com/b852b50a57.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>FoodMarket | Website Jual-Beli E-Book Koding</title>
+    <title>FoodMarket | Solusi Belanja Kebutuhan Makanan Menjadi Lebih Mudah</title>
 </head>
 
 <body>
@@ -33,7 +33,7 @@ if (!isset($_SESSION['user_login'])) {
             <div class="col-md-5 col-sm-5 col-7 my-1 px-0">
                 <form>
                     <div class="input-group">
-                        <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Apa yang sedang anda cari ?">
+                        <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Makanan Cepat Saji...">
                         <div class="input-group-prepend">
                             <a href="pencarian.html" class="input-group-text text-decoration-none"><i class="fas fa-search"></i></a>
                         </div>
@@ -83,19 +83,19 @@ if (!isset($_SESSION['user_login'])) {
                     <div class="carousel-item active">
                         <img src="img/slider1.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
-                            <h2>Baca dimanapun dan kapanpun</h2>
+                            <h2>Solusi Belanja Kebutuhan Makanan Menjadi Lebih Mudah</h2>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <img src="img/slider2.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
-                            <h2>Simpan Koleksi Anda dikomputasi Awan</h2>
+                            <h2>Belanja Dimana aja dan Kapan aja</h2>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <img src="img/slider3.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
-                            <h2>Akses koleksi anda kapanpun</h2>
+                            <h2>Belanja Menjadi Lebih Mudah</h2>
                         </div>
                     </div>
                 </div>
@@ -143,20 +143,20 @@ if (!isset($_SESSION['user_login'])) {
         <div class="row justify-content-center">
             <div class="mb-3 col-lg-4 col-md-4 col-5" style="max-width: 540px;">
                 <div class="text-center">
-                    <img src="img/paperless.png" alt="" srcset="">
+                    <img src="img/info1.png" alt="" srcset="" width="200">
                     <div class="card-body">
-                        <h3 class="card-title">Paperless</h3>
-                        <p class="card-text">Ikut selamatkan lingkungan dengan membaca secara paperless</p>
+                        <h3 class="card-title">Kapan Aja</h3>
+                        <!-- <p class="card-text">Belnaja Kapan Aja</p> -->
                     </div>
                 </div>
             </div>
             <div class="border"></div>
             <div class="mb-3 col-lg-4 col-md-4 col-5 pt-4" style="max-width: 540px;">
                 <div class="text-center">
-                    <img src="img/teratur.png" alt="" srcset="" width="">
+                    <img src="img/info2.png" alt="" srcset="" width="180">
                     <div class="card-body">
-                        <h3 class="card-title">Teratur</h3>
-                        <p class="card-text">Simpan koleksi anda secara rapi di komputasi awan</p>
+                        <h3 class="card-title">Dimana AJa</h3>
+                        <!-- <p class="card-text">Belanja Dimana Aja</p> -->
                     </div>
                 </div>
             </div>
@@ -191,72 +191,32 @@ if (!isset($_SESSION['user_login'])) {
         </div>
     </div>
     <div class="container mt-5">
-        <h4 class="text-primary">Web Development</h4>
-        <div class="card-columns web-developer row">
-            <div class="card bg-white text-white  col-lg-3 col-md-6 col-6">
+        <h4 class="text-primary">Kategori Makanan</h4>
+        <div class="card-columns web-developer row justify-content-md-center mt-2">
+            <?php
+                $foods_category = $database->getFoodCategory();
+                
+                while($food_category = mysqli_fetch_assoc($foods_category)) {
+            ?>
+            <div class="card bg-white text-white  col-lg-2 col-md-6 col-6">
                 <img src="img/html.webp" class="card-img" alt="...">
                 <div class="card-img-overlay">
-                    <h2 class="card-title text-center">HTML</h2>
+                    <h4 class="card-title text-center"><?= $food_category['name_category'] ?></h2>
                 </div>
             </div>
-            <div class="card bg-white text-dark  col-lg-3 col-md-6 col-6">
-                <img src="img/css.PNG" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h2 class="card-title text-center">CSS</h2>
-                </div>
-            </div>
-            <div class="card bg-white text-dark  col-lg-3 col-md-6 col-6">
-                <img src="img/js.png" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h2 class="card-title text-center">JAVASCRIPT</h2>
-                </div>
-            </div>
-            <div class="card bg-white text-dark  col-lg-3 col-md-6 col-6">
-                <img src="img/php.png" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h2 class="card-title text-center">PHP</h2>
-                </div>
-            </div>
+                <?php } ?>
+            
         </div>
     </div>
     <div class="container mt-5">
-        <h4 class="text-primary">Framework Terpopuler</h4>
-        <div class="card-columns web-developer row">
-            <div class="card bg-white text-white col-lg-3 col-md-6 col-6">
-                <img src="img/laravel.png" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h2 class="card-title text-center">LARAVEL</h2>
-                </div>
-            </div>
-            <div class="card bg-white text-dark col-lg-3  col-md-6 col-6">
-                <img src="img/react.png" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h2 class="card-title text-center">REACT JS</h2>
-                </div>
-            </div>
-            <div class="card bg-white text-dark col-lg-3  col-md col-6">
-                <img src="img/vue.jpg" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h2 class="card-title text-center">VUE JS</h2>
-                </div>
-            </div>
-            <div class="card bg-white text-dark col-lg-3  col-md-6 col-6">
-                <img src="img/flutter.png" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h2 class="card-title text-center">FLUTTER</h2>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container mt-5">
-        <h5 class="text-primary">Rasakan Sensasi Belanja Dengan Sangat Cepat Di Tom-Ebook</h5>
-        <small>Tom-Ebook merpakan salah satu perusahaan yang bergerak di bidang e-commerce di indonesia yang menual bebagai jenis E-book yang berkaitan dengan Koding. Kenapa E-book ? Karena secara tidak sengaja kita telah berkontribusi dalam penyelamatan lingkungan yang mana kita telah mengurangi penggunaan kertas dan tentu harga E-book lebih murah di banding buku. Di sini (Tom-Ebook) tersedia berbagai jenis metode pembayaran untuk memastikan kegiatan belanja anda dapat dilakukan dengan senyaman mungkin. Data pribadi dan seluruh transaksi yang pernah dan akan di lakukan akan di lindungi oleh kebijakan privasi Tom-Ebook. Tom-Ebook menjadi solusi untuk belanja online dengan mudah dan nyaman.</small>
+        <h5 class="text-primary">Rasakan Sensasi Belanja Dengan Sangat Cepat Di FoodMarket</h5>
+        <small>FoodMarket merupakan salah satu perusahaan yang bergerak di bidang e-commerce di indonesia yang menjual bebagai jenis Makanan. KDi sini (FoodMarket) tersedia berbagai jenis metode pembayaran untuk memastikan kegiatan belanja anda dapat dilakukan dengan senyaman mungkin. Data pribadi dan seluruh transaksi yang pernah dan akan di lakukan akan di lindungi oleh kebijakan privasi FoodMarket. FoodMarket menjadi solusi untuk belanja online dengan mudah dan nyaman.</small>
     </div>
     <div class="footer bg-light mt-5">
         <div class="container">
             <div class="row pt-3 justify-content-center">
                 <div class="col=md-3 col-4">
-                    <h5><b>TOM E-BOOK</b></h5>
+                    <h5><b>FoodMarket</b></h5>
                     <p>Tentang Kami</p>
                     <p>Blog</p>
                     <p>Keuangan</p>
@@ -269,15 +229,15 @@ if (!isset($_SESSION['user_login'])) {
                 </div>
                 <div class="col-md-3 col-4 text-decoration-none">
                     <h5><b>IKUTI KAMI</b></h5>
-                    <a href="https://www.instagram.com/" class="text-decoration-none"><i class="fab fa-instagram"></i></a>
-                    <a href="https://www.instagram.com/" class="text-decoration-none"><i class="fab fa-facebook"></i></i></a>
-                    <a href="https://www.instagram.com/" class="text-decoration-none"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-decoration-none"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-decoration-none"><i class="fab fa-facebook"></i></i></a>
+                    <a href="#" class="text-decoration-none"><i class="fab fa-twitter"></i></a>
                 </div>
             </div>
         </div>
         <hr>
         <div class="pb-3">
-            <p class="text-center">&copy;2018-2019 All Right Reserved. PT. Tomo Digital</p>
+            <p class="text-center">&copy;2020 All Right Reserved. PT. Tomo Digital</p>
         </div>
     </div>
 
